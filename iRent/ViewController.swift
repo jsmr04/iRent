c98f5f9a2ef687e292d86dd306d84ecebe5139d5
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2020-08-19 10:03:52 -0400

    Initial Commit

---
+++ iRent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRent
+//
+//  Created by Jose Smith Marmolejos on 2020-08-19.
+//  Copyright © 2020 Jose Smith Marmolejos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
