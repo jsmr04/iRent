3c6d2bd290ef9fe9a145a8d1560439f4ccb4327b
Arthur Ataide <arthurreisataide@gmail.com>
2020-08-22 09:20:26 -0400

    Personal View added.

---
+++ iRent/Controller/Menu/Personal/PersonalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PersonalViewController.swift
+//  iRent
+//
+//  Created by Arthur Ataide on 2020-08-21.
+//  Copyright © 2020 Jose Smith Marmolejos. All rights reserved.
+//
+
+import UIKit
+
+class PersonalViewController: UIViewController {
+
+    @IBOutlet weak var userImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userImage.layer.borderWidth = 1.0
+        userImage.layer.masksToBounds = false
+        userImage.layer.borderColor = UIColor.white.cgColor
+        userImage.layer.cornerRadius = userImage.frame.size.width/2
+        userImage.clipsToBounds = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
