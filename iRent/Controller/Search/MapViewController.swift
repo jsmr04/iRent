29c090f13fd35166b736d017cd95f1f85e0ac998
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2020-08-24 18:08:07 -0400

    Search by map

--- iRent/Controller/Search/MapViewController.swift
+++ iRent/Controller/Search/MapViewController.swift
@@ -33,6 +33,8 @@ class MapViewController: UIViewController {
         mapFloatingButton.backgroundColor = UIColor(named: "RedMain")
         mapFloatingButton.layer.cornerRadius = mapFloatingButton.frame.height / 2
         
+        mapView.delegate = self
+        
         searchBar.delegate = self
         
         searchBar.backgroundColor = UIColor.black.withAlphaComponent(0.1)
@@ -74,10 +76,10 @@ class MapViewController: UIViewController {
     
     @objc func logOut(){
         do {
-          try Auth.auth().signOut()
+            try Auth.auth().signOut()
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Login", style: .plain, target: self, action: #selector(goToLogin))
         } catch let signOutError as NSError {
-          print ("Error signing out: %@", signOutError)
+            print ("Error signing out: %@", signOutError)
         }
     }
     
@@ -96,9 +98,10 @@ class MapViewController: UIViewController {
         mapView.region = reg
     }
     
-    func addAnnotation(_ coordinate:CLLocationCoordinate2D) {
+    func addAnnotation(_ coordinate:CLLocationCoordinate2D, _ title:String) {
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
+        annotation.title = title
         annotation.subtitle = ""
         mapView.addAnnotation(annotation)
     }
@@ -106,93 +109,103 @@ class MapViewController: UIViewController {
     func getAreaCoordinates(){
         let rect = mapView.visibleMapRect
         let neMapPoint = MKMapPoint(x: rect.maxX, y: rect.origin.y)
-//        let swMapPoint = MKMapPoint(x: rect.origin.x, y: rect.maxY)
+        //        let swMapPoint = MKMapPoint(x: rect.origin.x, y: rect.maxY)
         let midMapPoint = MKMapPoint(x: rect.midX, y: rect.midY)
         
         let neCoord = neMapPoint.coordinate
-//        let swCoord = swMapPoint.coordinate
+        //        let swCoord = swMapPoint.coordinate
         let midCoord = midMapPoint.coordinate
         
         let c1 = CLLocation(latitude: midCoord.latitude, longitude: midCoord.longitude)
         let c2 = CLLocation(latitude: neCoord.latitude, longitude: neCoord.longitude)
-         
+        
         let distance = c1.distance(from: c2)
         
-            refProperty.child("property").observeSingleEvent(of: .value, with: { (snapshot) in
-                // Get user value
-                if let value = snapshot.value as? NSDictionary{
-                    print(value.allKeys)
-                    for key in value.allKeys{
-                        let p = value[key] as? NSDictionary
+        AppDelegate.shared().propertyList.removeAll()
+        
+        refProperty.child("property").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            if let value = snapshot.value as? NSDictionary{
+                print(value.allKeys)
+                for keyUser in value.allKeys{
+                    if let propertyJson = value[keyUser] as? NSDictionary{
                         
-                        let id = key as! String
-                        let title = p?["title"] as? String ?? ""
-                        let description = p?["description"] as? String ?? ""
-                        let location = p?["location"] as? String ?? ""
-                        
-                        if location.contains(","){
-                            let lat = location.split(separator: ",")[0]
-                            let long = location.split(separator: ",")[1]
+                        for propertyKey in propertyJson.allKeys{
                             
-                            let dLat = Double(lat)
-                            let dLong = Double(long)
-                            let propertyLocation = CLLocation(latitude: dLat!, longitude: dLong!)
+                            let p = propertyJson[propertyKey] as? NSDictionary
                             
-                            AppDelegate.shared().propertyList.removeAll()
+                            let id = propertyKey as! String
+                            let title = p?["title"] as? String ?? ""
+                            let description = p?["description"] as? String ?? ""
+                            let location = p?["location"] as? String ?? ""
                             
-                            let propertyDistance = propertyLocation.distance(from: c1)
-                            if propertyDistance <= distance{
-                                let propertyList = PropertyList()
-                                propertyList.id = id
-                                propertyList.title = title
-                                propertyList.description = description
-                                propertyList.location = location
+                            if location.contains(","){
+                                let lat = location.split(separator: ",")[0]
+                                let long = location.split(separator: ",")[1]
                                 
-                                AppDelegate.shared().propertyList.append(propertyList)
+                                let dLat = Double(lat)
+                                let dLong = Double(long)
+                                let propertyLocation = CLLocation(latitude: dLat!, longitude: dLong!)
+                                
+                                AppDelegate.shared().propertyList.removeAll()
+                                
+                                let propertyDistance = propertyLocation.distance(from: c1)
+                                if propertyDistance <= distance{
+                                    let propertyList = PropertyList()
+                                    propertyList.id = id
+                                    propertyList.title = title
+                                    propertyList.description = description
+                                    propertyList.location = location
+                                    
+                                    AppDelegate.shared().propertyList.append(propertyList)
+                                    
+                                    self.addAnnotation(propertyLocation.coordinate, title)
+                                }
                             }
                         }
-                        
-//                        //Getting first photo
-//                        self.refPhoto.child("propertyPhoto").observeSingleEvent(of: .value, with: { (snapshot) in
-//                            if let valuePhoto = snapshot.value as? NSDictionary{
-//                                for keyPhoto in valuePhoto.allKeys{
-//                                    let pp = valuePhoto[keyPhoto] as? NSDictionary
-//
-//                                    let imageString = pp?["photo"] as? String ?? ""
-//                                    let propertyId = pp?["propertyId"] as? String ?? ""
-//
-//                                    print("id \(id) | propertyId \(propertyId)")
-//                                    if id == propertyId{
-//                                        let propertyPhoto = PropertyPhoto()
-//                                        propertyPhoto.photoString = imageString
-//                                        self.propertyPhotos.append(propertyPhoto)
-//                                        self.tableView.reloadData()
-//                                        break
-//                                    }
-//                                }
-//                            }
-//                        }) { (error) in
-//                            print(error.localizedDescription)
-//                        }
-                        
-//                        self.properties.append(property)
                     }
+                    
+                    //                        //Getting first photo
+                    //                        self.refPhoto.child("propertyPhoto").observeSingleEvent(of: .value, with: { (snapshot) in
+                    //                            if let valuePhoto = snapshot.value as? NSDictionary{
+                    //                                for keyPhoto in valuePhoto.allKeys{
+                    //                                    let pp = valuePhoto[keyPhoto] as? NSDictionary
+                    //
+                    //                                    let imageString = pp?["photo"] as? String ?? ""
+                    //                                    let propertyId = pp?["propertyId"] as? String ?? ""
+                    //
+                    //                                    print("id \(id) | propertyId \(propertyId)")
+                    //                                    if id == propertyId{
+                    //                                        let propertyPhoto = PropertyPhoto()
+                    //                                        propertyPhoto.photoString = imageString
+                    //                                        self.propertyPhotos.append(propertyPhoto)
+                    //                                        self.tableView.reloadData()
+                    //                                        break
+                    //                                    }
+                    //                                }
+                    //                            }
+                    //                        }) { (error) in
+                    //                            print(error.localizedDescription)
+                    //                        }
+                    
+                    //                        self.properties.append(property)
                 }
-                
-                //self.tableView.reloadData()
-                
-            }) { (error) in
-                print(error.localizedDescription)
             }
+            
+            //self.tableView.reloadData()
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
         
         
         
-//        print("Coor: \(neCoord)")
-//        print("Coor: \(swCoord)")
+        //        print("Coor: \(neCoord)")
+        //        print("Coor: \(swCoord)")
         
-//        addAnnotation(neCoord)
-////        addAnnotation(swCoord)
-//        addAnnotation(midCoord)
+        //        addAnnotation(neCoord)
+        ////        addAnnotation(swCoord)
+        //        addAnnotation(midCoord)
     }
     
     func getMidPoint(){
@@ -203,28 +216,51 @@ class MapViewController: UIViewController {
 
 
 extension MapViewController:UIGestureRecognizerDelegate,MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        mapView.removeAnnotations(mapView.annotations)
+        getAreaCoordinates()
+    }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+          
+          let id = MKMapViewDefaultAnnotationViewReuseIdentifier
+          
+          if let v = mapView.dequeueReusableAnnotationView(
+              withIdentifier: id, for: annotation) as? MKMarkerAnnotationView {
+              v.canShowCallout = true
+              v.image = #imageLiteral(resourceName: "logo1_small")
+              v.markerTintColor = UIColor.clear
+              v.glyphTintColor = UIColor.clear
+              return v
+          }
+          return nil
+      }
 }
 
 extension MapViewController:CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-       
+        
         if let location = locations.first {
             goTo(location.coordinate)
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to find user's location: \(error.localizedDescription)")
     }
+    
+    
 }
 
 extension MapViewController:UISearchBarDelegate{
     
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         print("Search: \(searchBar.text!)")
-        getAreaCoordinates()
         
         if let searchText = searchBar.text{
             let request = MKLocalSearch.Request()
