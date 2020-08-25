266ba74bb58bd5ce1f84e4153856131d11f83e51
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2020-08-25 13:35:42 -0400

    adjustments

---
+++ iRent/Model/Application.swift
@@ -0,0 +1,18 @@
+//
+//  Application.swift
+//  iRent
+//
+//  Created by Jose Smith Marmolejos on 2020-08-25.
+//  Copyright © 2020 Jose Smith Marmolejos. All rights reserved.
+//
+
+import UIKit
+public class Application {
+    var id:String = ""
+    var propertyId:String = ""
+    var created:String = ""
+    var offer:String = ""
+    var comment:String = ""
+    var applicantName:String = ""
+    
+}
